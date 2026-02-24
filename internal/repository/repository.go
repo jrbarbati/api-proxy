e11578f790850759a420207908402e0008157563
= <=>
2026-02-23 20:56:53 -0500

    dropping Repository interface as it wasn't being used

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,10 +3,3 @@ package repository
 type Identifiable interface {
 	GetID() int
 }
-
-type Repository[T any] interface {
-	FindByID(id int) (*T, error)
-	Update(data *T) (*T, error)
-	Insert(data *T) (*T, error)
-	Delete(id int) error
-}

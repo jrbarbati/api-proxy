bf7804ebc9c7296226c904b768d6c77a5c5b4a3d
= <=>
2026-03-01 04:23:54 -0500

    adding omit empty to client secret and user passwords" gp

--- internal/model/internal_user.go
+++ internal/model/internal_user.go
@@ -5,7 +5,7 @@ import "time"
 type InternalUser struct {
 	ID            int        `json:"id"`
 	Email         string     `json:"email"`
-	Password      string     `json:"password"`
+	Password      string     `json:"password,omitempty"`
 	CreatedAt     time.Time  `json:"created_at"`
 	UpdatedAt     *time.Time `json:"updated_at"`
 	InactivatedAt *time.Time `json:"inactivated_at"`
--- internal/model/service_account.go
+++ internal/model/service_account.go
@@ -7,7 +7,7 @@ type ServiceAccount struct {
 	OrgID         int        `json:"org_id"`
 	Identifier    string     `json:"identifier"`
 	ClientID      string     `json:"client_id"`
-	ClientSecret  string     `json:"client_secret"`
+	ClientSecret  string     `json:"client_secret,omitempty"`
 	CreatedAt     time.Time  `json:"created_at"`
 	UpdatedAt     *time.Time `json:"updated_at"`
 	InactivatedAt *time.Time `json:"inactivated_at"`
